f99493ba2c412b17fd5cb649ace5a851d9134c31
Jared Holzman <jared@holzman.io>
2016-11-04 23:17:54 -0400

    Get video player working

--- src/main/webapp/src/ui/component/VideoPlayerComponent.js
+++ src/main/webapp/src/ui/component/VideoPlayerComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { default as Video, Controls, Play, Mute, Seek, Fullscreen, Time, Overlay } from 'react-html5video';
+import {Row, Col, Button} from 'react-bootstrap';
+import {default as Video, Controls, Play, Mute, Seek, Fullscreen, Time, Overlay} from 'react-html5video';
 import 'react-html5video/dist/ReactHtml5Video.css';
 
 export default class VideoPlayerComponent extends Component {
@@ -8,22 +9,52 @@ export default class VideoPlayerComponent extends Component {
         data: React.PropTypes.object
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+        this.video = null;
+    }
+
+
+    reloadVideo() {
+        // When changing a HTML5 video, you have to reload it.
+        this.video.load();
+        this.video.play();
+    }
 
     render() {
-        console.log(this.props.data.videoURL);
         return (
-            <Video controls autoPlay loop muted>
-                <source src={'http://media.w3.org/2010/05/sintel/trailer.mp4'}
-                        type="video/mp4" />
-                <Overlay />
-                <Controls>
-                    <Play />
-                    <Seek />
-                    <Time />
-                    <Mute />
-                    <Fullscreen />
-                </Controls>
-            </Video>
+            <div>
+                <Row>
+                    <Col md={12}>
+                        <Video controls
+                               autoPlay
+                               loop
+                               muted
+                               ref={(video) => this.video = video}>
+                            <source src={this.props.data.videoURL}/>
+                            <Overlay />
+                            <Controls>
+                                <Play />
+                                <Seek />
+                                <Time />
+                                <Mute />
+                                <Fullscreen />
+                            </Controls>
+                        </Video>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <Button onClick={() => {
+                            console.log(this);
+                            this.reloadVideo();
+                        }}>Reload</Button>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
